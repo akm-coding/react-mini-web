ceb5723c1581a5c385092a0a4d54e3aad1fa1aae
naylynnmyat9504 <aungkaungmyatyer123@gmail.com>
2023-05-04 13:55:44 +0630

    React Mini Web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./components/Hero";
+import ImageSlider from "./components/ImageSlider";
+import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
+import Plan from "./components/Plan";
+import Rooms from "./components/Rooms";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <Offers />
+      <Plan />
+      <Rooms />
+      <ImageSlider />
+      <Footer />
     </div>
   );
 }
